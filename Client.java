cbcdca216dc2f9dcf9600a0f689bdfa1f94b0a12
Kiran Mal <kiranmalpmvk@gmail.com>
2024-04-02 17:43:07 +0530

    chat App

---
+++ Client.java
@@ -0,0 +1,76 @@
+import java.io.*;
+
+import java.net.*;
+
+public class Client {
+    Socket socket;
+
+    BufferedReader br;
+    PrintWriter out;
+
+    public Client() {
+        try {
+            System.out.println("request to server");
+            socket = new Socket("127.0.0.1", 7777);
+            System.out.println("Connection done");
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startReading() {
+        Runnable r1 = () -> {
+            System.out.println("reader started");
+            try {
+                while (true) {
+                    String msg = br.readLine();
+                    if (msg.equals("exit")) {
+                        System.out.println("client terminate the chat");
+
+                        socket.close();
+                        break;
+                    }
+                    System.out.println("Client:" + msg);
+
+                }
+            } catch (Exception e) {
+                System.out.println("connection closed");
+            }
+        };
+        new Thread(r1).start();
+    }
+
+    public void startWriting() {
+        Runnable r2 = () -> {
+            System.out.println("writer started");
+            try {
+                while ( !socket.isClosed()) {
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                    String content = br1.readLine();
+                    out.println(content);
+                    out.flush();
+                    if(content.equals("exit")){
+                        socket.close();
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+        };
+        new Thread(r2).start();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("this is client...");
+        new Client();
+    }
+}
